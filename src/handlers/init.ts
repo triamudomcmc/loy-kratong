92df6f0c94d736edf3da2f023db27eb52397dd9e
betich <betich365@gmail.com>
2021-11-10 19:27:38 +0700

    :sparkles: สนตอ

--- src/handlers/init.ts
+++ src/handlers/init.ts
@@ -1,5 +1,7 @@
-import {BridgeContext, createBridgeContext} from "next-bridge";
-import {ResultData} from "@components/Kratong/create";
-import {ResultData as PResultData} from "@components/Principal/create";
+import { BridgeContext, createBridgeContext } from "next-bridge";
+import { ResultData } from "@components/Kratong/create";
+import { ResultData as PResultData } from "@components/Principal/create";
+import { PSAResultData } from "@pages/cupsa";
 
-export const sendDataContext: BridgeContext<{id: string, data: ResultData | PResultData}> = createBridgeContext("sendData", "send")
+export const sendDataContext: BridgeContext<{ id: string; data: ResultData | PResultData | PSAResultData }> =
+  createBridgeContext("sendData", "send");
