ddb692040c977a40c9f259886ccb7aac3e96b093
betich <betich365@gmail.com>
2021-11-12 16:04:23 +0700

    :wrench: fix cupsaa context

--- src/handlers/init.ts
+++ src/handlers/init.ts
@@ -1,7 +1,7 @@
 import { BridgeContext, createBridgeContext } from "next-bridge";
 import { ResultData } from "@components/Kratong/create";
 import { ResultData as PResultData } from "@components/Principal/create";
-import { PSAResultData } from "@pages/CUPSAA";
+import { ResultData as CResultData } from "@components/CUPSAA/create";
 
-export const sendDataContext: BridgeContext<{ id: string; data: ResultData | PResultData | PSAResultData }> =
+export const sendDataContext: BridgeContext<{ id: string; data: ResultData | PResultData | CResultData }> =
   createBridgeContext("sendData", "send");
