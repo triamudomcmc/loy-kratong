32895b50dd37a6dd94ddbd89a142ec8f5adcb7ac
betich <betich365@gmail.com>
2021-11-18 20:15:52 +0700

    window href

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,18 +2,16 @@ import { Meta } from "@components/Meta";
 import { Navbar } from "@components/Nav";
 import { Story } from "@components/Story";
 import type { NextPage } from "next";
-import {useEffect} from "react";
+import { useEffect } from "react";
 import Router from "next/router";
 
 const Home: NextPage = () => {
-
   useEffect(() => {
-    const id = localStorage.getItem("entityId")
-    if (id === "" || !id) return
+    const id = localStorage.getItem("entityId");
+    if (id === "" || !id) return;
 
-    Router.push("/loykratong")
-
-  }, [])
+    window.location.href = "/loykratong";
+  }, []);
 
   return (
     <>
