82d831b21fd0d2b57ec33a3998e9c1747c9b08bd
pryter <peterphongpak@gmail.com>
2021-11-19 04:10:06 +0700

    Add filter page && timestamp

--- src/bridges/filter/action/loadList.ts
+++ src/bridges/filter/action/loadList.ts
@@ -10,6 +10,13 @@ export const loadListAction = loadListContext.helper.createAction(async (ApiPara
   }
 
   const data = await init.collection("rawEntities").limit(20).get()
+  const raw = data.docs.length > 0 ? data.docs.map((item) => ({...item.data(), id: item.id})) : []
 
-  return {status: true, report: "success", data: data.docs.length > 0 ? data.docs.map((item) => ({...item.data(), id: item.id})) : []}
+  const ard = await init.collection("archivedEntities").limit(20).get()
+  const archived = ard.docs.length > 0 ? ard.docs.map((item) => ({...item.data(), id: item.id})) : []
+
+  const show = await init.collection("filteredEntities").limit(20).get()
+  const display = show.docs.length > 0 ? show.docs.map((item) => ({...item.data(), id: item.id})) : []
+
+  return {status: true, report: "success", data: {raw: raw, archived: archived, display: display}}
 })
